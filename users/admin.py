41fa2a5cdca8bc94ad354d3513ffb724f7e2146f
M-Hassan_Maqsood <hassanios665@gmail.com>
2025-09-09 12:35:25 +0500

    update readme file

--- users/admin.py
+++ users/admin.py
@@ -1,24 +1,9 @@
 from django.contrib import admin
 from users.models import User
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 
 @admin.register(User)
-class CustomUserAdmin(BaseUserAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "email", "phone")
     search_fields = ("username", "email", "phone", "first_name", "last_name")
     list_filter = ("is_staff", "is_superuser", "is_active")
-
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("username", "phone", "date_of_birth", "password1", "password2",),
-        }),
-    )
