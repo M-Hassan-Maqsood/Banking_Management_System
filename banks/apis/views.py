48134163d3501922d70571d407e7a54e881dcf8d
M-Hassan_Maqsood <hassanios665@gmail.com>
2025-09-12 19:17:49 +0500

    add phase 4 in project

--- banks/apis/views.py
+++ banks/apis/views.py
@@ -1,25 +1,14 @@
-from rest_framework import viewsets, generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.db.models import Count
+from rest_framework import generics
 
 from banks.models import Bank
 from banks.serializers import BankSerializer
 
 
-class BankListAPIView(APIView):
-    def get(self, request):
-        banks = Bank.objects.all().annotate(branch_count = Count("branches"))
-        serializer = BankSerializer(banks, many = True)
-
-        return Response(serializer.data)
-
-
-class BankGenericAPIView(generics.ListAPIView):
+class BankListCreateAPIView(generics.ListCreateAPIView):
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
 
 
-class BankViewSet(viewsets.ReadOnlyModelViewSet):
+class BankDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Bank.objects.all()
     serializer_class = BankSerializer
