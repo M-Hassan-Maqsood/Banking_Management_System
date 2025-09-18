e90afdafcd4d0525a15b4402624ae3700b6346a3
M-Hassan-Maqsood <hassanios665@gmail.com>
2025-09-18 18:06:35 +0500

    remove line space

--- BMS/settings.py
+++ BMS/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
     # Custom middleware to restrict user access during maintenance mode
     "BMS.middleware.MaintenanceModeMiddleware",
 ]
