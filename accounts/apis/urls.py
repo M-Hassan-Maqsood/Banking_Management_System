ab10df0ca0d89811805a325fc089477a2b844525
M-Hassan-Maqsood <hassanios665@gmail.com>
2025-09-24 12:25:02 +0500

    add phase 10 in project

--- accounts/apis/urls.py
+++ accounts/apis/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accounts.apis.views import AccountListCreateAPIView, AccountBalanceUpdateAPIView, StaffAccountDetailAPIView
+from accounts.apis.views import AccountListCreateAPIView, AccountBalanceUpdateAPIView, StaffAccountDetailAPIView, SummaryAPIView
 
 
 urlpatterns = [
     path("", AccountListCreateAPIView.as_view(), name = "account-list-create-api"),
     path("<int:pk>/balance/", AccountBalanceUpdateAPIView.as_view(), name = "account-balance-api"),
     path("<int:pk>/", StaffAccountDetailAPIView.as_view(), name = "staff-detail-api"),
+    path("reports/<int:pk>/summary/", SummaryAPIView.as_view(), name = "summary-api"),
 ]
--- accounts/apis/views.py
+++ accounts/apis/views.py
@@ -1,11 +1,19 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView
+import datetime
+from decimal import Decimal
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, UpdateAPIView, RetrieveAPIView
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.filters import OrderingFilter, SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Case, When, Sum, Max, DecimalField, F, Value, Min
+from django.db.models.functions import Coalesce
+from django.db.models import Window
+from rest_framework.response import Response
+from rest_framework import status
 
+from BMS.choices import AccountType, AmountType
 from accounts.apis.permissions import IsStaffUser
-from accounts.serializers import AccountSerializer
-from accounts.models import Account
+from accounts.serializers import AccountSerializer, AccountSummarySerializer
+from accounts.models import Account, Transaction
 
 
 class AccountListCreateAPIView(ListCreateAPIView):
@@ -38,3 +46,100 @@ class StaffAccountDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     permission_classes = [IsStaffUser]
+
+
+class SummaryAPIView(RetrieveAPIView):
+    def retrieve(self, request, *args, **kwargs):
+        year = request.query_params.get("year")
+        month = request.query_params.get("month")
+        account_id = kwargs.get("pk")
+
+        account = Account.objects.get(id = account_id)
+        if not account:
+            return Response({"Account does not found"},status=status.HTTP_404_NOT_FOUND)
+
+        if month and year:
+            start_date = datetime.date(int(year), int(month), 1)
+        elif year:
+            start_date = datetime.date(int(year), 1, 1)
+        else:
+            start_date = None
+
+        txn = Transaction.objects.filter(account_id = account_id)
+
+        if year:
+            txn = txn.filter(date__year = year)
+        if month:
+            txn = txn.filter(date__month = month)
+
+        report = txn.aggregate(
+            deposit_case = Coalesce(
+                Sum(
+                Case(
+                    When(type = AmountType.Deposit.value, then = F("amount")),
+                    default = Value(0),
+                    output_field = DecimalField()
+                )
+                    ), Value(Decimal("0.00"))
+            ),
+
+            withdrawal_case=Coalesce(Sum(
+                Case(
+                    When(type = AmountType.Withdrawal.value, then = F("amount")),
+                    default = Value(0),
+                    output_field = DecimalField()
+                )
+            ), Value(Decimal("0.00"))
+            ),
+            max_txn_amount = Coalesce(Max("amount"),Value(Decimal("0.00")))
+        )
+
+        annotate = txn.annotate(
+            running_balance= Coalesce(Window(
+                expression = Sum(
+            Case(
+                When(type = AmountType.Deposit.value, then = F("amount")),
+                When(type = AmountType.Withdrawal.value, then = F("amount")),
+                default = Value(0),
+                output_field = DecimalField(),
+            )
+        ),
+            order_by = ["date","id"]
+                ), Value(Decimal("0.00"))
+            )
+        )
+
+        opening_balance = Transaction.objects.filter(
+            account_id = account_id,
+            date__lt = start_date,
+        ).aggregate(
+            balance = Coalesce(
+                Sum(
+                Case(
+                When(type = AmountType.Deposit.value, then = F("amount")),
+                When(type = AmountType.Withdrawal.value, then = -F("amount")),
+                default = Value(0),
+                output_field = DecimalField(),
+                )
+                ), Value(Decimal("0.00"))
+            )
+        )["balance"]
+
+        lowest_balance = annotate.aggregate(
+            min_balance = Coalesce(
+            Min("running_balance"),
+                Value(Decimal("0.00"))
+            )
+        )["min_balance"]
+
+        data = {
+            "account_id": account.id,
+            "opening_balance": opening_balance,
+            "total_deposits": report["deposit_case"],
+            "total_withdrawals": report["withdrawal_case"],
+            "max_txn_amount": report["max_txn_amount"],
+            "min_running_balance": lowest_balance,
+        }
+
+        serializer = AccountSummarySerializer(data)
+        return Response(serializer.data, status = status.HTTP_200_OK)
